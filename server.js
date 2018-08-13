b263a62048daed594ec46f8f7734316b4be5fad2
Ferperemero86 <fernandoperezmerono@gmail.com>
2018-08-13 20:55:30 +0100

    Add comments

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const fs = require('fs');
 
 var app = express();
 
+//Acces partials to avoid code repition using templating engine.
 hbs.registerPartials(__dirname + '/views/partials');
+
+//Set the format of the templating engine(handlebars).
 app.set('view engine', 'hbs');
 
+//Acces static files like html and css documents.
 app.use(express.static(__dirname + '/public'));
 
+//Use helper functions to avoid repetition with handlebars
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
@@ -17,6 +22,7 @@ hbs.registerHelper('capitalLetters', (text) => {
   return text.toUpperCase();
 });
 
+//Use handlebars in a dynamic way with render method adding an object with properties
 app.get('/about',(req,res) => {
   res.render('about.hbs', {
       pageTitle: 'About Page'
@@ -29,6 +35,7 @@ app.get('/',(req,res) => {
     });
 });
 
+//Call listen method to send a response to the browser and see the content in the port specified
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
